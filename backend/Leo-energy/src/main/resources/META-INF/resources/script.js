ad75a10b6e52eb6133ffdc95d7e18abdb68520b9
markSuuS <m.breinesberger@students.htl-leonding.ac.at>
2023-12-11 16:17:37 +0100

    1st prototype of frontend with example data (not from server)

--- backend/Leo-energy/src/main/resources/META-INF/resources/script.js
+++ backend/Leo-energy/src/main/resources/META-INF/resources/script.js
@@ -1,110 +1,92 @@
-window.onload = fetchDeviceData;
-let jsonData = null;
-const sortValuesElement = document.getElementById('sortValues');
-const descendingElement = document.getElementById('descending');
+const date = document.getElementById("date");
+const submitButton = document.getElementById("submitBtn");
 
-function fetchDeviceData(){
-    fetch('/device/data')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(json => {
-            jsonData = json;
-            createDevicesTable(json.devices);
-            createValuesTable(json.values);
-            createValueTypesTable(json.valueTypes);
-            createUnitsTable(json.units);
-        })
-        .catch(error => console.error('Error:', error));
-}
+submitButton.addEventListener("click", () => {
+    const dateValueMap = fetchData(date.value);
 
-function createDevicesTable(devices) {
-    const devicesTable = document.getElementById('devicesTable').getElementsByTagName('tbody')[0];
-
-    for (const device of devices) {
-        const row = devicesTable.insertRow();
-        row.insertCell(0).textContent = device.id;
-        row.insertCell(1).textContent = device.name;
-        row.insertCell(2).textContent = device.site;
-    }
-}
-
-function createValuesTable(values) {
-    const valuesTable = document.getElementById('valuesTable').getElementsByTagName('tbody')[0];
-    valuesTable.innerHTML = "";
-
-    for (const value of values) {
-        const row = valuesTable.insertRow();
-        row.insertCell(0).textContent = value.id;
-        row.insertCell(1).textContent = value.time;
-        row.insertCell(2).textContent = value.value;
-        row.insertCell(3).textContent = value.valueType.description;
-        row.insertCell(4).textContent = value.valueType.unit.name;
-    }
-}
-
-function createValueTypesTable(valueTypes) {
-    const valueTypesTable = document.getElementById('valueTypesTable').getElementsByTagName('tbody')[0];
-
-    for (const valueType of valueTypes) {
-        const row = valueTypesTable.insertRow();
-        row.insertCell(0).textContent = valueType.id;
-        row.insertCell(1).textContent = valueType.description;
-        row.insertCell(2).textContent = valueType.presistValues;
-        row.insertCell(3).textContent = valueType.sentomqtt;
-        row.insertCell(4).textContent = valueType.unit.id;
-    }
-}
-
-function createUnitsTable(units) {
-    const unitsTable = document.getElementById('unitsTable').getElementsByTagName('tbody')[0];
-
-    for (const unit of units) {
-        const row = unitsTable.insertRow();
-        row.insertCell(0).textContent = unit.id;
-        row.insertCell(1).textContent = unit.name;
-    }
-}
-
-sortValuesElement.addEventListener('change', function () {
-    const sortBy = this.value;
-    sortValuesTable(sortBy);
+    const chart = new CanvasJS.Chart('chartContainer', {
+        axisX: {
+            valueFormatString: 'HH:mm',
+            interval: 1,
+            labelAngle: -50,
+            title: 'Time HH:mm'
+        },
+        axisY: {
+            title: 'W'
+        },
+        data: [{
+            type: 'line',
+            dataPoints: Array.from(dateValueMap, ([date, value]) => ({
+                x: date,
+                y: value
+            }))
+        }]
+    });
+    chart.render();
 });
 
-descendingElement.addEventListener('change', function () {
-    const sortBy = sortValuesElement.value;
-    sortValuesTable(sortBy);
-})
-function sortValuesTable(sortBy) {
-    const checkbox = document.getElementById('descending');
-    jsonData.values.sort((a, b) => {
 
-        let aValue, bValue;
-
-        if (sortBy === 'valueType') {
-            aValue = a.valueType.description;
-            bValue = b.valueType.description;
-        } else if (sortBy === 'unit') {
-            aValue = a.valueType.unit.name;
-            bValue = b.valueType.unit.name;
-        } else {
-            aValue = a[sortBy];
-            bValue = b[sortBy];
-        }
-
-        if(checkbox.checked === true){
-            const bubble = aValue;
-            aValue = bValue;
-            bValue = bubble;
-        }
-
-        if (aValue < bValue) { return -1; }
-        if (aValue > bValue) { return 1; }
-        return 0;
-    });
-
-    createValuesTable(jsonData.values);
+function fetchData(dayIso){
+    //TODO:
+    //fetch the data from the quarkus application from the specific day from 0:00 am to 11:59pm
+    //@param dayIso: the day where we want to get the data
+    //@returns a key-value collection (e.g. Map) with the time as key and the watt-value as value
+    return fillExampleDataToMap();
 }
+
+function fillExampleDataToMap(){
+    const map = new Map();
+    map.set(new Date(2023,9,1,0,0,5), 70);
+    map.set(new Date(2023,9,1,0,45,5), 30);
+    map.set(new Date(2023,9,1,2,0,5), 85);
+    map.set(new Date(2023,9,1,3,0,5), 70);
+    map.set(new Date(2023,9,1,4,45,5), 30);
+    map.set(new Date(2023,9,1,6,23,5), 85)
+    map.set(new Date(2023,9,1,20,23,5), 1);
+    return map;
+}
+
+function averageOfCollection(collection){
+    let sum = 0;
+
+    for(var item of collection){
+        sum += item;
+    }
+
+    return sum/collection.size;
+}
+
+
+
+
+
+/*fetch('https://localhost:8080/device/data')
+.then(response => response.json())
+.then(data => {
+    createChart(data, 'monthlyChart', 'Monthly');
+    createChart(data, 'dailyChart', 'Daily');
+})
+
+function createChart(data, chartId, title) {
+    const ctx = document.getElementById(chartId).getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.labels,
+            datasets: data.datasets
+        },
+        options: {
+            title: {
+                display: true,
+                text: title
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}*/
