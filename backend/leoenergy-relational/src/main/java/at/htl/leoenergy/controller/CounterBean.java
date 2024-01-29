9a9d9aca056a587cdc2d3ba19e4df881f8affd6f
BalintB4 <balazsbalint513@gmail.com>
2024-01-29 15:26:35 +0100

    244biot-35 changed to full graph

--- backend/leoenergy-relational/src/main/java/at/htl/leoenergy/controller/CounterBean.java
+++ backend/leoenergy-relational/src/main/java/at/htl/leoenergy/controller/CounterBean.java
@@ -33,10 +33,10 @@ public class CounterBean {
     }
 
 
-  @Scheduled(every="5s")
+ /* @Scheduled(every="5s")
     void increment() {
         fileProcessorHelper.importJsonFiles(directoryNameAll,20);
         //LIMIT for the datas how many it should read at once
-    }
+    }*/
 
 }
