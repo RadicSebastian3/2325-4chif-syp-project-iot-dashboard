38ee0a198aa318a7163317d447d24554448bfc84
markSuuS <m.breinesberger@students.htl-leonding.ac.at>
2024-02-09 10:55:10 +0100

    244biot-41 extended mqtt receiver for SmartHome/Sun_Power/...

--- backend/leoenergy-relational/src/main/java/at/htl/leoenergy/Mqtt/MqttReceiver.java
+++ backend/leoenergy-relational/src/main/java/at/htl/leoenergy/Mqtt/MqttReceiver.java
@@ -1,18 +1,8 @@
 package at.htl.leoenergy.Mqtt;
 
 import io.quarkus.logging.Log;
-import io.smallrye.reactive.messaging.mqtt.MqttMessage;
-import io.smallrye.reactive.messaging.mqtt.MqttMessageMetadata;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 @ApplicationScoped
 public class MqttReceiver {
@@ -23,10 +13,6 @@ public class MqttReceiver {
     private static final String productionWTopic = "production-w";
     private static final String battRemainingCapacityWhTopic = "batt-remaining-capacity-wh";
     private static final String battRemainingCapacityPercentTopic = "batt-remaining-capacity-%";
-
-    @Inject
-    @Channel("merged-sunpower-data")
-    Emitter<SunpowerData> mergedSunpowerData;
 
     @Incoming(timestampTopic)
     public void receiveTimestamp(byte[] byteArray){
@@ -62,33 +48,5 @@ public class MqttReceiver {
     public void receiveBattRemainingCapacityPercent(byte[] byteArray){
         final String msgAsJson = new String(byteArray);
         Log.info("Received batt remainung capacity %: " + msgAsJson);
-    }
-
-    private void processMqttMessagePart(String value, String topic) {
-        SunpowerData energyData = new SunpowerData();
-        energyData.setTimeStamp(LocalDateTime.now());  // Setzen Sie den Zeitstempel nach Bedarf
-        switch (topic) {
-            case "timestamp":
-                energyData.setTimeStamp(LocalDateTime.parse(value.trim(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-                break;
-            case "consumption":
-                energyData.setConsumptionW(Double.parseDouble(value.trim()));
-                break;
-            case "gridFeedIn":
-                energyData.setGridFeedInW(Double.parseDouble(value.trim()));
-                break;
-            case "production":
-                energyData.setProductionW(Double.parseDouble(value.trim()));
-                break;
-            case "battRemainingCapacityWh":
-                energyData.setBattRemainingCapacityWh(Double.parseDouble(value.trim()));
-                break;
-            case "battRemainingCapacityPercent":
-                energyData.setBattRemainingCapacityPercent(Double.parseDouble(value.trim()));
-                break;
-        }
-
-        // Sende das EnergyData-Objekt an den Emitter
-        mergedSunpowerData.send(energyData);
     }
 }
