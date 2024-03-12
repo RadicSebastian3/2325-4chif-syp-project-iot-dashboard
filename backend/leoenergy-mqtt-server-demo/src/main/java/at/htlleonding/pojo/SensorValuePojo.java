459b450ad47ed92269138bd616cacfad5a89c783
ErikSeifried <erikseifried007@gmail.com>
2024-03-12 08:30:37 +0100

    #244biot-68 Test mqtt-demo-server SensorValuePojo

--- backend/leoenergy-mqtt-server-demo/src/main/java/at/htlleonding/pojo/SensorValuePojo.java
+++ backend/leoenergy-mqtt-server-demo/src/main/java/at/htlleonding/pojo/SensorValuePojo.java
@@ -1,7 +1,6 @@
 package at.htlleonding.pojo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class SensorValuePojo {
     @JsonProperty("deviceName")
     public String deviceName;
