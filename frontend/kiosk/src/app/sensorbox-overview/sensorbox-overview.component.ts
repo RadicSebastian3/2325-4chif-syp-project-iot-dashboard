81c1b5c4bcfa86a7a96c4d3b7f15d736763b09cf
ErikSeifried <erikseifried007@gmail.com>
2024-12-25 22:20:03 +0100

    Implement detail view for sensor boxes with fake data integration

--- frontend/kiosk/src/app/sensorbox-overview/sensorbox-overview.component.ts
+++ frontend/kiosk/src/app/sensorbox-overview/sensorbox-overview.component.ts
@@ -1,23 +1,33 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SensorBoxDTO} from "../model/SensorBoxDTO";
 import {SensorboxService} from "../services/sensorbox.service";
-import {NgForOf} from "@angular/common";
+import {NgClass, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-sensorbox-overview',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf,
+    NgOptimizedImage,
+    NgClass
   ],
   templateUrl: './sensorbox-overview.component.html',
   styleUrl: './sensorbox-overview.component.css'
 })
 export class SensorboxOverviewComponent implements OnInit, OnDestroy{
-  public floors: string[] = [];
-  public rooms: string[] = [];
-  public currentSensorboxValues: Map<string, SensorBoxDTO> = new Map();
-  private intervalId: any;
+  public floors: string[] = ["EG", "OG1", "OG2"];
+  public rooms: string[] = ["EG01", "EG02", "OG101", "OG102", "OG201", "OG202"];
+  public currentSensorboxValues: Map<string, SensorBoxDTO> = new Map([
+    ['EG01', new SensorBoxDTO('EG01', 'EG', Date.now(), 350, 45, 1, 100, 40, 1010, -50, 20)],
+    ['EG02', new SensorBoxDTO('EG02', 'EG', Date.now(), 1200, 25, 0, 200, 60, 1005, -55, 12)],
+    ['OG101', new SensorBoxDTO('OG101', 'OG1', Date.now(), 800, 50, 1, 150, 45, 1020, -40, 22)],
+    ['OG102', new SensorBoxDTO('OG102', 'OG1', Date.now(), 300, 55, 1, 150, 35, 1015, -60, 18)],
+    ['OG201', new SensorBoxDTO('OG201', 'OG2', Date.now(), 700, 30, 1, 100, 50, 1018, -45, 19)],
+    ['OG202', new SensorBoxDTO('OG202', 'OG2', Date.now(), 1500, 20, 0, 200, 70, 1002, -65, 14)]
+  ]);
 
+  private intervalId: any;
   private openFloors: Set<string> = new Set();
   private openRooms: Set<string> = new Set();
 
@@ -74,17 +84,32 @@ export class SensorboxOverviewComponent implements OnInit, OnDestroy{
 
   getTemperatureIcon(temperature?: number): string {
     return temperature && temperature < 18
-      ? 'assets/icons/temperature-cold.png'
-      : 'assets/icons/temperature-hot.png';
+      ? 'assets/images/temperature-cold.png'
+      : 'assets/images/temperature-hot.png';
   }
 
   getHumidityIcon(humidity?: number): string | null {
     if (humidity === undefined || humidity === null) return null;
-    if (humidity < 30) return 'assets/icons/humidity-low.png';
-    if (humidity <= 60) return 'assets/icons/humidity-medium.png';
-    if (humidity > 60) return 'assets/icons/humidity-high.png';
+    if (humidity < 30) return 'assets/images/humidity-low.png';
+    if (humidity <= 60) return 'assets/images/humidity-medium.png';
+    if (humidity > 60) return 'assets/images/humidity-high.png';
     return null;
   }
+
+  getCo2Class(co2?: number): string {
+    if (co2 === undefined || co2 === null) return '';
+    if (co2 < 400) return 'low';
+    if (co2 <= 1000) return 'medium';
+    return 'high';
+  }
+
+  getNoiseClass(noise?: number): string {
+    if (noise === undefined || noise === null) return '';
+    if (noise < 50) return 'low';
+    if (noise <= 70) return 'medium';
+    return 'high';
+  }
+
 
   private loadFakeData(): void {
     this.floors = ['EG', 'OG1', 'OG2'];
@@ -112,31 +137,30 @@ export class SensorboxOverviewComponent implements OnInit, OnDestroy{
   //PLEASE DON'T TOUCH!!!
   //loads all floors and rooms, and syncs the latest values of all rooms
   ngOnInit() {
-    this.loadFakeData();
 
-    this.sbs.getAllFloors().subscribe((data) => {
-      this.floors = data;
-      console.log("All floors: " + this.floors);
-    });
-
-    this.sbs.getAllRooms().subscribe((data) => {
-      this.rooms = data;
-      console.log("All rooms: " + this.rooms);
-    });
-
-    this.intervalId = setInterval(() => {
-      this.rooms.forEach(room => {
-        this.sbs.getLatestValuesOfRoom(room).subscribe((data) => {
-          this.currentSensorboxValues.set(data.room, data);
-          console.log(this.currentSensorboxValues);
-        });
-      });
-
-    }, 10000);
+    // this.sbs.getAllFloors().subscribe((data) => {
+    //   this.floors = data;
+    //   console.log("All floors: " + this.floors);
+    // });
+    //
+    // this.sbs.getAllRooms().subscribe((data) => {
+    //   this.rooms = data;
+    //   console.log("All rooms: " + this.rooms);
+    // });
+    //
+    // this.intervalId = setInterval(() => {
+    //   this.rooms.forEach(room => {
+    //     this.sbs.getLatestValuesOfRoom(room).subscribe((data) => {
+    //       this.currentSensorboxValues.set(data.room, data);
+    //       console.log(this.currentSensorboxValues);
+    //     });
+    //   });
+    //
+    // }, 10000);
   }
 
   ngOnDestroy() {
-    this.intervalId ? clearInterval(this.intervalId) : null;
+    // this.intervalId ? clearInterval(this.intervalId) : null;
   }
   //#endregion
 }
