f08da3cc2d8406d77fa32ec70b09b9a1d4a95e78
ErikSeifried <erikseifried007@gmail.com>
2024-12-25 19:57:27 +0100

    Add method to count the green rooms for a floor, where all windows are closed

--- frontend/kiosk/src/app/sensorbox-overview/sensorbox-overview.component.ts
+++ frontend/kiosk/src/app/sensorbox-overview/sensorbox-overview.component.ts
@@ -61,6 +61,10 @@ export class SensorboxOverviewComponent implements OnInit, OnDestroy{
     return this.getRoomsForFloor(floor).length;
   }
 
+  getGreenRoomCount(floor: string): number {
+    return this.getRoomsForFloor(floor).filter((room) => !this.isWindowOpen(room)).length;
+  }
+
   //#region Service
   //PLEASE DON'T TOUCH!!!
   //loads all floors and rooms, and syncs the latest values of all rooms
